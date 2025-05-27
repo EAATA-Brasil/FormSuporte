33abf1f72538ab39ed71ca4fe9d10a378883d4a5
EAATA-Brasil <yago.eaatabrasil@gmail.com>
2025-05-27 14:21:29 -0300

    add suporte

--- situacao_veiculo/models.py
+++ situacao_veiculo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from dateutil.relativedelta import relativedelta
+from django.utils import timezone
 
-# Create your models here.
+class Cliente(models.Model):
+    data = models.DateField(verbose_name="Data",blank=False, default=timezone.now)
+    vencimento = models.DateField(verbose_name="Vencimento", blank=True, null=True)
+    serial = models.CharField(verbose_name="Serial", max_length=100, blank=True, null=True)
+    nome = models.CharField(verbose_name="Nome", max_length=100, blank=False)
+    cnpj = models.CharField(max_length=30, verbose_name='CPF/CNPJ', blank=True, default="SEM DADO")
+    tel = models.CharField(max_length=30, verbose_name='Telefone', blank=True, default="SEM DADO")
+    email = models.EmailField(verbose_name='Email', blank=True)
+    feedback = models.CharField(verbose_name='Feedback', blank=True, max_length=255, default="")
+    equipamento = models.CharField(verbose_name='Equipamento', max_length=100, blank=False)
+
+    def save(self, *args, **kwargs):
+        if self.data and not self.vencimento:
+            self.vencimento = self.data + relativedelta(years=2)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.nome} - {self.vencimento}"
--- situacao_veiculo/urls.py
+++ situacao_veiculo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index_situacao'),
+    path('', views.index, name='index'),
+    path('buscar/', views.buscar_serial, name='buscar_serial'),
 ]
