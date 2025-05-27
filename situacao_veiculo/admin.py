eee599b63612173a313705e6693fcda557d0c09a
EAATA-Brasil <yago.eaatabrasil@gmail.com>
2025-05-27 18:12:42 -0300

    modify admin/atendimento_suporte

--- situacao_veiculo/admin.py
+++ situacao_veiculo/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Cliente
 
-# Register your models here.
-admin.site.register(Cliente)
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('serial', 'nome')
+    search_fields = ('serial','nome')
