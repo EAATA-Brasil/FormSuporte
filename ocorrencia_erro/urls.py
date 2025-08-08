d575f8791552c50ff254c860eb03c7a860628d08
EAATA-Brasil <yago.eaatabrasil@gmail.com>
2025-08-08 09:23:31 -0300

    tratamento de erro

--- ocorrencia_erro/urls.py
+++ ocorrencia_erro/urls.py
@@ -5,11 +5,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('subir_ocorrencia/', views.subir_ocorrencia, name='subir_ocorrencia'),
     path('filter_data/', views.filter_data_view, name='filter_data'),
-    path('update_ocorrencia/', views.update_record_view, name='update_ocorrencia/'),
+    path('update_ocorrencia/', views.update_record_view, name='update_ocorrencia'),
     path('criar_usuario/', views.criar_usuario, name='criar_usuario'),
     path('login/', views.login_view, name='login_ocorrencias'),  
     path('logout/', views.logout_view, name='logout'),
-    path('download_arquivo/<int:arquivo_id>/', views.download_arquivo, name='download_arquivo/'),
+    path('download_arquivo/<int:arquivo_id>/', views.download_arquivo, name='download_arquivo'),
     path('get_record/<int:pk>/', views.get_record, name='get_record'),
 
 ]
