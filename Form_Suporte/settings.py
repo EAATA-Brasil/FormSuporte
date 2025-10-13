52997b5a0b735ca89016434864163b37ad8088e6
EAATA-Brasil <yago.eaatabrasil@gmail.com>
2025-10-13 16:43:03 -0300

    corrigindo falhas de segurança

--- Form_Suporte/settings.py
+++ Form_Suporte/settings.py
@@ -46,7 +46,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6u$z58nj9!@tqbyx5$f103&-jj3^%1ve=p@z@aomo89#ugn6wp'
+SECRET_KEY = os.getenv('SECRET_KEY', ''),
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
