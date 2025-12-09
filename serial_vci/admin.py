8cdd3a7085c864afec3dbb329fc81dea6af43a15
EAATA-Brasil <yago.eaatabrasil@gmail.com>
2025-12-09 10:23:13 -0300

    modify admin

--- serial_vci/admin.py
+++ serial_vci/admin.py
@@ -7,7 +7,14 @@ class FotosInline(admin.TabularInline):
 
 @admin.register(SerialVCI)
 class SerialVCIAdmin(admin.ModelAdmin):
-    list_display = ("numero_vci", "cliente", "email", "pedido")
+    list_display = ("id", "numero_vci", "cliente", "email", "pedido")
+    search_fields = ("id", "numero_vci", "cliente", "email")
+    list_filter = ("cliente", "email")
+    ordering = ("-id",)
     inlines = [FotosInline]
 
-admin.site.register(SerialFoto)
+
+@admin.register(SerialFoto)
+class SerialFotoAdmin(admin.ModelAdmin):
+    list_display = ("id", "serial", "imagem")
+    search_fields = ("id", "serial__numero_vci")
